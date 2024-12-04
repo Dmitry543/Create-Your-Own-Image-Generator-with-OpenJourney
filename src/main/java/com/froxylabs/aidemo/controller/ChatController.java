e7bf80044bf914f2fceebd56f832d73da982e202
Dmitry543 <dmitry.b.third@gmail.com>
2024-12-04 11:43:20 +0300

    Added environment variable. Modified the controller.

--- src/main/java/com/froxylabs/aidemo/controller/ChatController.java
+++ src/main/java/com/froxylabs/aidemo/controller/ChatController.java
@@ -2,34 +2,31 @@ package com.froxylabs.aidemo.controller;
 
 import com.froxylabs.aidemo.client.HuggingFaceModelClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.Map;
-
-@RestController
+@Controller
 @RequestMapping("/ai")
 public class ChatController {
-    private final String ACCESS_TOKEN = "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";
+
+    @Value("${access.token}")
+    private String accessToken;
 
     private HuggingFaceModelClient client;
 
     @Autowired
     public ChatController() {
         this.client = HuggingFaceModelClient.builder()
-                .modelName("google-t5/t5-small")
-                .accessToken(ACCESS_TOKEN)
+                .modelName("prompthero/openjourney-v4")
+                .accessToken(accessToken)
                 .maxLength(100)
                 .maxRetries(5)
                 .retryDelay(1000)
+                .xWaitForModel(true)
+                .numInferenceSteps(10)
                 .build();
     }
 
-    @GetMapping("/generate")
-    public Map generate(@RequestParam(value = "message", defaultValue = "I love coffee") String message) throws IOException {
-        return Map.of("generation", client.call(message));
-    }
 }
