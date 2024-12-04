4e2b65045d410d176d3acc6d280522262292f5a6
Dmitry543 <dmitry.b.third@gmail.com>
2024-12-04 11:31:23 +0300

    Added new parameters and headers to the request.

--- src/main/java/com/froxylabs/aidemo/client/HuggingFaceModelClient.java
+++ src/main/java/com/froxylabs/aidemo/client/HuggingFaceModelClient.java
@@ -18,15 +18,19 @@ public class HuggingFaceModelClient {
 
     private final String modelName;
     private final String accessToken;
+
     private final int maxLength;
     private final Double temperature;
+    private final boolean xWaitForModel;
+    private final int numInferenceSteps;
+
     private final int maxRetries;
     private final long retryDelay;
 
     @Builder.Default
     private final OkHttpClient client = new OkHttpClient();
 
-    public String call(String inputs) throws IOException {
+    public String call(String inputs,String negativePrompt) throws IOException {
         //Check if inputs are not empty
         if (inputs.isEmpty()) {
             throw new IllegalArgumentException("Input string cannot be empty");
@@ -36,29 +40,38 @@ public class HuggingFaceModelClient {
         Map<String, Object> payload = new HashMap<>();
         payload.put("inputs", inputs);
 
+        //Build payload parameters
         Map<String, Object> parameters = new HashMap<>();
+
         parameters.put("max_length",maxLength);
         if (temperature != null) {
             parameters.put("temperature", temperature);
         }
-        payload.put("parameters", parameters);
+        parameters.put("num_inference_steps", numInferenceSteps);
+        parameters.put("negative_prompt",negativePrompt);
 
+        payload.put("parameters", parameters);
         String requestParams = objectMapper.writeValueAsString(payload);
+
         //Create the complete url
         String url = API_URL + modelName;
-        //Building the request and sending it
+        //Build the request and add headers
         RequestBody requestBody = RequestBody.create(requestParams, MediaType.parse("application/json"));
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("Authorization","Bearer " + accessToken)
+                .addHeader("x-wait-for-model", String.valueOf(xWaitForModel))
                 .post(requestBody)
                 .build();
 
+        //Send the request
         int retries = 0;
         while (true) {
             try (Response response = client.newCall(request).execute()) {
                 if(response.isSuccessful()) {
                     return response.body().string();
+                } else if(response.code() == 503) {
+                    System.out.println("----------> Response code was 503!!!!");
                 } else if (retries< maxRetries) {
                     retries++;
                     try {
