a5a15a6344b33da29ea65febe6aec8b777d75196
Dmitry543 <dmitry.b.third@gmail.com>
2024-12-04 11:52:21 +0300

    Added method GET.

--- src/main/java/com/froxylabs/aidemo/controller/ChatController.java
+++ src/main/java/com/froxylabs/aidemo/controller/ChatController.java
@@ -1,11 +1,21 @@
 package com.froxylabs.aidemo.controller;
 
 import com.froxylabs.aidemo.client.HuggingFaceModelClient;
+import com.froxylabs.aidemo.model.Generation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
 
 @Controller
 @RequestMapping("/ai")
@@ -27,6 +37,14 @@ public class ChatController {
                 .xWaitForModel(true)
                 .numInferenceSteps(10)
                 .build();
+    }
+
+    @GetMapping("/generate")
+    public String showGenerationPage(Model model) {
+        model.addAttribute("generation", new Generation());
+        model.addAttribute("prompt", "");
+
+        return "index";
     }
 
 }
